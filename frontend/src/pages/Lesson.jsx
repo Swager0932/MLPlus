a4534f66d732302829fc432bac7bf014825891d0
Swager0932 <swagerbot2000@gmail.com>
2023-09-12 08:22:30 -0400

    lesson fetch Fix

--- frontend/src/pages/Lesson.jsx
+++ frontend/src/pages/Lesson.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-import {   } from "../adapters/lesson-adapter";
+import { getLesson } from "../adapters/lesson-adapter";
 
 // let URL = "https://www.youtube.com/embed/6gHfVQ50OnQ?si=ti_apvfSIrjFX4S6";
 let transcriptTitle = "Description"
