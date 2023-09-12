b2235eb8e9b029b5652eb11409681a7f974ca5f0
Swager0932 <swagerbot2000@gmail.com>
2023-09-11 23:55:06 -0400

    irrelavent

---
+++ src/db/models/lesson.js
@@ -0,0 +1,67 @@
+const knex = require('../knex');
+const { hashPassword, isValidPassword } = require('../../utils/auth-utils');
+
+class Lesson {
+  #passwordHash = null; // a private property
+
+  // Why have a constructor here? We need a way to take the raw data returned from 
+  // the database and hide the passwordHash before sending it back to the controller
+  constructor({ id, username, password_hash }) {
+    this.id = id;
+    this.username = username;
+    this.#passwordHash = password_hash;
+  }
+
+  static async list() {
+    const query = 'SELECT * FROM users';
+    const { rows } = await knex.raw(query);
+    return rows.map((user) => new Lesson(user)); // use the constructor to hide each user's passwordHash
+  }
+
+  static async find(id) {
+    const query = 'SELECT * FROM users WHERE id = ?';
+    const args = [id];
+    const { rows } = await knex.raw(query, args);
+    const user = rows[0];
+    return user ? new Lesson(user) : null; 
+  }
+
+  static async findByUsername(username) {
+    const query = 'SELECT * FROM users WHERE username = ?';
+    const args = [username];
+    const { rows } = await knex.raw(query, args);
+    const user = rows[0];
+    return user ? new Lesson(user) : null;
+  }
+
+  static async create(username, password) {
+    const passwordHash = await hashPassword(password);
+
+    const query = `INSERT INTO users (username, password_hash)
+      VALUES (?, ?) RETURNING *`;
+    const args = [username, passwordHash];
+    const { rows } = await knex.raw(query, args);
+    const lesson = rows[0];
+    return new Lesson(lesson);
+  }
+
+  static async deleteAll() {
+    return knex.raw('TRUNCATE users;');
+  }
+
+  update = async (username) => { // dynamic queries are easier if you add more properties
+    const rows = await knex('users')
+      .where({ id: this.id })
+      .update({ username })
+      .returning('*');
+
+    const updatedUser = rows[0];
+    return updatedUser ? new Lesson(updatedUser) : null;
+  };
+
+  isValidPassword = async (password) => (
+    isValidPassword(password, this.#passwordHash)
+  );
+}
+
+module.exports = Lesson;
