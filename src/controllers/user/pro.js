a2e2a2d723d555db0f14228bbf320d2860ef7e0e
Swager0932 <swagerbot2000@gmail.com>
2023-10-18 18:33:52 -0400

    tried to implament pfp

---
+++ src/controllers/user/pro.js
@@ -0,0 +1,21 @@
+const { isAuthorized } = require('../../utils/auth-utils');
+
+const updateUser = async (req, res) => {
+  const {
+    session, // this req.session property is put here by the handleCookieSessions middleware
+    db: { User }, // this req.db.User property is put here by the addModelsToRequest middleware
+    body: { pfp }, // this req.body property is put here by the client
+    params: { id }, // this req.params.id is a part of the request URL
+  } = req;
+
+  if (!isAuthorized(id, session)) return res.sendStatus(403);
+
+  const user = await User.find(id);
+  if (!user) return res.sendStatus(404);
+
+  const updatedPfp = await user.pfp(pfp);
+  res.send(updatedPfp);
+
+};
+
+module.exports = updateUser;
